7acc80692caec6711e785e8e05e9d15f01f522ca
Tamas Balogh <tamas.balogh@i-new.com>
2020-07-16 17:19:36 +0200

    image upload

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -7,24 +7,37 @@
 <!--            </v-row>-->
 <!--        </v-app-bar>-->
         <v-container fluid fill-height class="bg-img">
-            <v-row>
-                <div class="upload-button">
-                    <div class="upload-button-content">
-                        <div class="upload-button-top">1</div>
-                        <div class="upload-button-bottom">2</div>
-                    </div>
-                </div>
-                <v-col style="height: 200px; width: 200px">col1.1</v-col>
-            </v-row>
-            <v-row>
-                <v-col>col2.1</v-col>
-                <v-col>col2.2</v-col>
-            </v-row>
-
-            <v-row align="center" justify="center">
-                <v-col><v-btn>Képek feltöltése<v-icon right>mdi-upload</v-icon></v-btn></v-col>
-                <v-col><v-btn>Képek Facebookról<v-icon right>mdi-facebook</v-icon></v-btn></v-col>
-                <v-col><v-btn>Képek Instagramról<v-icon right>mdi-instagram</v-icon></v-btn></v-col>
+            <v-row justify="center">
+                <v-flex class="xs6 md3 lg2">
+                    <v-card class="pa-4">
+                        <v-row>
+                            <v-col>
+                                <div>
+                                    <v-hover v-slot:default="{ hover }">
+                                        <v-card :elevation="hover ? 10 : 2" align="center" class="pa-4" v-on:click="imageUpload()">
+                                            <p>Képek feltöltése</p>
+                                            <v-icon large>mdi-upload</v-icon>
+                                            <input multiple ref="uploader" class="d-none" type="file" accept="image/*" @change="onFileChanged()">
+                                        </v-card>
+                                    </v-hover>
+                                </div>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col>
+                                <v-hover v-slot:default="{ hover }">
+                                    <v-card :elevation="hover ? 10 : 2" align="center" class="pa-4" v-on:click="imageOnline()">
+                                        <p>Online képek</p>
+                                        <v-row>
+                                            <v-col><v-icon large>mdi-facebook</v-icon></v-col>
+                                            <v-col><v-icon large>mdi-instagram</v-icon></v-col>
+                                        </v-row>
+                                    </v-card>
+                                </v-hover>
+                            </v-col>
+                        </v-row>
+                    </v-card>
+                </v-flex>
             </v-row>
         </v-container>
 <!--        <v-footer class="grey darken-2" absolute padless>-->
@@ -43,9 +56,20 @@
         name: "Order",
         data () {
             return {
+                selectedFiles: []
             }
         },
         methods: {
+            imageUpload(){
+                this.$refs.uploader.click()
+            },
+            imageOnline(){
+
+            },
+            onFileChanged(){
+                this.selectedFiles = this.$refs.uploader.files
+                console.log(this.selectedFiles.length)
+            }
         }
     }
 
@@ -62,12 +86,6 @@
     border-style: solid;
     border-radius: 15px;
     animation: blinkingBackground 3s infinite;
-}
-
-.upload-button-content {
-    position: absolute;
-    width: 100%;
-    height: 50%;
 }
 
 @keyframes blinkingBackground{
