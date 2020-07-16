4b95a434183e9b94bd5bfd33744673344abc54f9
tbalogh <baloght@gain.uni-sopron.hu>
2020-07-16 23:40:12 +0200

    image uploader

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -12,15 +12,13 @@
                     <v-card class="pa-4">
                         <v-row>
                             <v-col>
-                                <div>
-                                    <v-hover v-slot:default="{ hover }">
-                                        <v-card :elevation="hover ? 10 : 2" align="center" class="pa-4" v-on:click="imageUpload()">
-                                            <p>Képek feltöltése</p>
-                                            <v-icon large>mdi-upload</v-icon>
-                                            <input multiple ref="uploader" class="d-none" type="file" accept="image/*" @change="onFileChanged()">
-                                        </v-card>
-                                    </v-hover>
-                                </div>
+                                <v-hover v-slot:default="{ hover }">
+                                    <v-card :elevation="hover ? 10 : 2" align="center" class="pa-4" v-on:click="onInputClick()">
+                                        <p>Képek feltöltése</p>
+                                        <v-icon large>mdi-upload</v-icon>
+                                        <input ref="uploader" class="d-none" type="file" accept="image/*" @change="onImagePick()">
+                                    </v-card>
+                                </v-hover>
                             </v-col>
                         </v-row>
                         <v-row>
@@ -39,6 +37,24 @@
                     </v-card>
                 </v-flex>
             </v-row>
+
+            <v-dialog v-model="isImagePicked" persistent max-width="600px">
+                <v-card>
+                    <Cropper ref="cropper" class="cropper" :src="selectedImage" :stencil-props="{ aspectRatio: 1/1 }"/>
+                    <v-card-actions>
+                        <v-btn color="green darken-1" text @click="doCrop()">Mentés</v-btn>
+                        <v-spacer/>
+                        <v-btn color="red darken-1" text @click="isImagePicked = false">Elvetés</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+
+            <v-row justify="center">
+                <v-col v-for="image in croppedImages" :key="image" class="col-3">
+                    <v-img v-if="croppedImages != null" :src="image"></v-img>
+                </v-col>
+            </v-row>
+
         </v-container>
 <!--        <v-footer class="grey darken-2" absolute padless>-->
 <!--            <v-row>-->
@@ -52,24 +68,48 @@
 </template>
 
 <script>
+
+    import { Cropper } from 'vue-advanced-cropper'
+
     export default {
         name: "Order",
         data () {
             return {
-                selectedFiles: []
+                selectedImages: [],
+                selectedImage: null,
+
+                croppedImages: [],
+                croppedImage: null,
+
+                isImagePicked: false,
             }
         },
         methods: {
-            imageUpload(){
+            //trick because button acts as input
+            onInputClick(){
                 this.$refs.uploader.click()
             },
-            imageOnline(){
-
+            //called after the image picked in input
+            onImagePick(){
+                this.selectedImage = this.$refs.uploader.files
+                if (this.$refs.uploader.files && this.$refs.uploader.files[0]) {
+                    var reader = new FileReader();
+                    reader.readAsDataURL(this.$refs.uploader.files[0]);
+                    reader.onload = (e) => {
+                        this.selectedImage = e.target.result;
+                        this.isImagePicked = true
+                    };
+                }
             },
-            onFileChanged(){
-                this.selectedFiles = this.$refs.uploader.files
-                console.log(this.selectedFiles.length)
+            doCrop(){
+                const { coordinates, canvas } = this.$refs.cropper.getResult()
+                console.log(coordinates)
+                this.croppedImages.push(canvas.toDataURL())
+                this.isImagePicked = false
             }
+        },
+        components: {
+            Cropper
         }
     }
 
@@ -90,6 +130,11 @@
 
 @keyframes blinkingBackground{
     50%		{ border-color: orange;}
+}
+
+.cropper {
+    height: 400px;
+    background: #DDD;
 }
 
 </style>
